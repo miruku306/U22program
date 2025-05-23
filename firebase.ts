2058b4fde34bbbbd7ef6becb75183911519fbd9d
yuuki0405 <155883339+yuuki0405@users.noreply.github.com>
2025-05-23 09:59:01 +0900

    firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "xxx",
+  authDomain: "xxx.firebaseapp.com",
+  projectId: "xxx",
+  ...
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
